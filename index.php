ae7835915e028867d027d0e9a769860d7f482a68
Stephan <vanaf1979@gmail.com>
2019-03-25 15:24:33 +0100

    Added comments

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+
+<html>
+
+  <head>
+      
+      <?php wp_head(); ?>
+
+  </head>
+
+  <body <?php body_class(); ?>>
+
+    <div id="app">
+
+      <h1>FunctionsPhp</h1>
+
+    </div>
+
+    <?php wp_footer(); ?>
+
+  </body>
+
+</html>
